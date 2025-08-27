145e689ee278659cc74100a58238c43229091096
Lukas Nagel <github@lna-dev.net>
2025-08-27 19:29:52 +0200

    Init

---
+++ open_media_station_admin/android/app/src/main/kotlin/com/example/open_media_station_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.open_media_station_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ open_media_station_admin/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:open_media_station_base/helpers/app_helper.dart';
+
+void main(List<String> args) {
+    AppHelper.start(args, Placeholder(), "Open Media Station");
+}
